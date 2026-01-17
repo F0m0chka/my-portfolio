3d6f472cdc8dcf4cd9311df418080b3010ecb2c2
F0m0chka <maxfomin2008@gmail.com>
2026-01-17 13:38:49 +0100

    SECURITY PATCH: upgrade next to 15.1.9 per CVE-2025-66478

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Это разрешит билд, даже если в коде ошибки типов (красные линии)
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Это пропустит проверку правил написания кода
-    ignoreDuringBuilds: true,
-  },
-};
-
+  }
+}
 export default nextConfig;
